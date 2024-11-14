0c4d3927afa84b58d7add6d8df47a4b04f25e963
caponed <caponed@NewTest.myguest.virtualbox.org>
2024-11-14 18:38:21 -0400

    Adds some functionality to database driver

--- database_driver/__init__.py
+++ database_driver/__init__.py
@@ -1,4 +1,4 @@
 # Functions to export
-from .mongodb_driver import insert_pantry_essentials
+from .mongodb_driver import insert_pantry_essentials, get_pantry_essentials
 
-__all__ = ["insert_pantry_essentials"]
+__all__ = ["insert_pantry_essentials", "get_pantry_essentials"]
