853f15db83415f5b828892e3cd81fafd17098fc3
caponed <caponed@Ubuntu20Template.myguest.virtualbox.org>
2024-11-16 13:38:32 -0400

    Makes basic changes to python scripts and requirements.txt

--- database_driver/mongodb_driver.py
+++ database_driver/mongodb_driver.py
@@ -7,18 +7,24 @@ try:
     client = MongoClient('mongodb://localhost:27017/')
     db = client['RecipeDatabase']
 
-    # Set up all collections objects
+    # Set up all collection objects
     pantry_essentials_collection = db['pantryEssentials']
+    recipe_link_collection = db['recipeLinks']
+    saved_recipe_collection = db['savedRecipes']
     print("Successfully connected to MongoDB database")
 except Exception as e:
     print(f"MongoDB Error: {str(e)}")
 
 
-def insert_pantry_essentials(ingredient_item):
+def insert_pantry_essentials(ingredient_list: dict):
+    """
+    Inserts an ingredient list to be stored as pantry essentials
+    :param ingredient_list: dict containing list of ingredients
+    """
     try:
         # Delete existing item and add new item
         pantry_essentials_collection.delete_many({})
-        result = pantry_essentials_collection.insert_one(ingredient_item)
+        result = pantry_essentials_collection.insert_one(ingredient_list)
         print(f"Inserted item with ID: {result.inserted_id}")
     except Exception as e:
         # Print the type of the exception and the exception message
@@ -26,17 +32,25 @@ def insert_pantry_essentials(ingredient_item):
         print(f"Exception message: {str(e)}")
 
 
-def get_pantry_essentials():
+def get_pantry_essentials() -> dict:
+    """
+    Retrieves pantry essentials from database
+    :return: dict holding list of ingredients
+    """
     try:
-        result = pantry_essentials_collection.find_one()
-        return dumps(result)
+        return pantry_essentials_collection.find_one()
     except Exception as e:
         # Print the type of the exception and the exception message
         print(f"Exception type: {type(e)}")
         print(f"Exception message: {str(e)}")
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    ingredients_list = {
+        "ingredients": ["eggs", "milk", "flour", "salt", "pepper"]
+    }
+
+    insert_pantry_essentials(ingredients_list)
 
     pantry_essentials = get_pantry_essentials()
-    print(pantry_essentials)
+    print(type(pantry_essentials))
